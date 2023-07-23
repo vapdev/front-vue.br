6a26156bf688ed975e64bedc74ddea44ae6a6978
vitor <vpbohn@gmail.com>
2023-07-22 21:43:48 -0300

    googlesigninbutton

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,14 @@ export default defineNuxtConfig({
   pages: true,
   css: ['~/assets/css/main.css'],
 
-  modules: ['nuxt-icon'],
+  modules: [
+    'nuxt-icon',
+    'nuxt-vue3-google-signin'
+  ],
+
+  googleSignIn: {
+    clientId: '776141756180-jt5qfpq16gn5so6ui4ena0v8ijvaeman.apps.googleusercontent.com'
+  },
 
   postcss: {
     plugins: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
-<script setup>
+<script setup lang="ts">
+import {
+	GoogleSignInButton,
+	type CredentialResponse,
+} from 'vue3-google-signin';
+
+const handleLoginSuccess = (response: CredentialResponse) => {
+	const { credential } = response;
+	console.log("access token: " + credential);
+};
+
+const handleLoginError = () => {
+	console.log('logi failed');
+};
+
 </script>
 <template>
 	<div class="flex flex-col h-screen w-full justify-between">
 		<div class="flex flex-col h-1/3 mx-6">
 			<TheHeader />
 		</div>
+		<GoogleSignInButton
+			@success="handleLoginSuccess"
+			@error="handleLoginError"
+		></GoogleSignInButton>
 		<div class="flex">
 			<NuxtPage />
 		</div>
